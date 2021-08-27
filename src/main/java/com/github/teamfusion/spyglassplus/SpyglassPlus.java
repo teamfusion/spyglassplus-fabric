24be020882eb53e7f2591ad56e0592fd770c903d
RobertoHD <53623234+RobertoProductions@users.noreply.github.com>
2021-08-27 19:35:23 +0200

    Initial setup

---
+++ src/main/java/com/github/teamfusion/spyglassplus/SpyglassPlus.java
@@ -0,0 +1,10 @@
+package com.github.teamfusion.spyglassplus;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SpyglassPlus implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+	}
+}
