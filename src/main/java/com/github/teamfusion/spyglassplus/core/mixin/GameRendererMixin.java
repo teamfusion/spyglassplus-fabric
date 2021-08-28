7c3bff2f9b7e9468a38338f6e9a147315996a013
RobertoHD <53623234+RobertoProductions@users.noreply.github.com>
2021-08-28 15:49:53 +0200

    Scrutiny Enchantment
    Orcinus
    
    Co-Authored-By: 0rc1nus <76800218+0rc1nus@users.noreply.github.com>

--- src/main/java/com/github/teamfusion/spyglassplus/core/mixin/GameRendererMixin.java
+++ src/main/java/com/github/teamfusion/spyglassplus/core/mixin/GameRendererMixin.java
@@ -1,11 +1,16 @@
 package com.github.teamfusion.spyglassplus.core.mixin;
 
 import com.github.teamfusion.spyglassplus.core.registry.SpyglassPlusEnchantments;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -18,6 +23,24 @@ public class GameRendererMixin {
         int i = EnchantmentHelper.getLevel(SpyglassPlusEnchantments.ILLUMINATING, entity.getActiveItem());
         if (((PlayerEntity) entity).isUsingSpyglass() && i > 0) {
             cir.setReturnValue(1.0F);
+        }
+    }
+
+    @Shadow private float lastMovementFovMultiplier;
+
+    @Shadow private float movementFovMultiplier;
+    @Inject(at = @At("RETURN"), method = "getFov", cancellable = true)
+    public void getFOV(Camera camera, float tickDelta, boolean changingFov, CallbackInfoReturnable<Double> cir) {
+        MinecraftClient minecraftClient = MinecraftClient.getInstance();
+        ClientPlayerEntity player = minecraftClient.player;
+        if (player != null) {
+            int i = EnchantmentHelper.getLevel(SpyglassPlusEnchantments.SCRUTINY, player.getActiveItem());
+            if (player.isUsingSpyglass() && i > 0) {
+                double value = 6D - i;
+                double lerpvalue = MathHelper.lerp(value, this.movementFovMultiplier, this.lastMovementFovMultiplier);
+                lerpvalue += 8.0D + (i - 1.0D) * -1.0D;
+                cir.setReturnValue(lerpvalue);
+            }
         }
     }
 
