e852c63fa4f1b90eb20e212ff40e52ba2a51a496
RobertoHD <53623234+RobertoProductions@users.noreply.github.com>
2021-08-29 16:59:45 +0200

    Update CommandEnchantment.java

--- src/main/java/com/github/teamfusion/spyglassplus/common/enchantments/CommandEnchantment.java
+++ src/main/java/com/github/teamfusion/spyglassplus/common/enchantments/CommandEnchantment.java
@@ -5,4 +5,24 @@ public class CommandEnchantment extends SPEnchantment{
     public CommandEnchantment() {
         super(Rarity.COMMON);
     }
+
+    @Override
+    public int getMinPower(int level) {
+        return 15;
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 1;
+    }
+
+    @Override
+    public boolean isAvailableForEnchantedBookOffer() {
+        return true;
+    }
+
+    @Override
+    public boolean isAvailableForRandomSelection() {
+        return false;
+    }
 }
