2e830c7145eeee6e86440a7343efbe617a884edb
0rc1nus <mitchellctlui@gmail.com>
2021-08-29 21:25:28 +0800

    Added Command Enchantment

--- src/main/java/com/github/teamfusion/spyglassplus/core/mixin/SpyglassItemMixin.java
+++ src/main/java/com/github/teamfusion/spyglassplus/core/mixin/SpyglassItemMixin.java
@@ -7,21 +7,31 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.passive.FoxEntity;
+import net.minecraft.entity.passive.IronGolemEntity;
+import net.minecraft.entity.passive.SnowGolemEntity;
+import net.minecraft.entity.passive.TameableEntity;
+import net.minecraft.entity.passive.WolfEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ProjectileUtil;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SpyglassItem;
+import net.minecraft.predicate.entity.EntityPredicates;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 
+import java.util.List;
 import java.util.function.Predicate;
 
 @Mixin(SpyglassItem.class)
 public class SpyglassItemMixin extends Item {
+    private int commandTicks = 100;
+    private boolean isCommanded = false;
+    private boolean initiallyCommanded = false;
 
     public SpyglassItemMixin(Settings settings) {
         super(settings);
@@ -35,6 +45,26 @@ public class SpyglassItemMixin extends Item {
     @Override
     public int getEnchantability() {
         return 1;
+    }
+
+    public boolean isCommanded() {
+        return this.isCommanded;
+    }
+
+    public void setCommanded(boolean commanded) {
+        this.isCommanded = commanded;
+    }
+
+    public void setCommandTicks(int commandTicks) {
+        this.commandTicks = commandTicks;
+    }
+
+    public int getCommandTicks() {
+        return this.commandTicks;
+    }
+
+    public void setInitiallyCommanded(boolean initiallyCommanded) {
+        this.initiallyCommanded = initiallyCommanded;
     }
 
     @Override
@@ -52,6 +82,66 @@ public class SpyglassItemMixin extends Item {
                     StatusEffectInstance effectInstance = new StatusEffectInstance(StatusEffects.GLOWING, 2, 1, false, false, false);
                     if (entity != null) {
                         ((LivingEntity) entity).addStatusEffect(effectInstance);
+                    }
+                }
+                int k = EnchantmentHelper.getLevel(SpyglassPlusEnchantments.COMMAND, stack);
+                if (k > 0) {
+                    Entity entity = checkEntity(user, 64.0D);
+                    if (entity != null) {
+                        this.setInitiallyCommanded(true);
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity user, int slot, boolean selected) {
+        if (this.initiallyCommanded) {
+            this.commandTicks--;
+            if (this.getCommandTicks() == 0) {
+                this.setCommanded(true);
+                this.setInitiallyCommanded(false);
+                this.setCommandTicks(100);
+                if (this.isCommanded()) {
+                    this.setCommanded(false);
+                    Entity entity = checkEntity((LivingEntity) user, 64.0D);
+                    Box box = new Box(user.getBlockPos()).expand(6.0D);
+                    List<WolfEntity> nearbyWolves = world.getEntitiesByClass(WolfEntity.class, box, TameableEntity::isTamed);
+                    List<IronGolemEntity> nearbyIronGolems = world.getEntitiesByClass(IronGolemEntity.class, box, EntityPredicates.EXCEPT_CREATIVE_OR_SPECTATOR);
+                    List<SnowGolemEntity> nearbySnowGolems = world.getEntitiesByClass(SnowGolemEntity.class, box, EntityPredicates.EXCEPT_CREATIVE_OR_SPECTATOR);
+                    List<FoxEntity> nearbyFoxes = world.getEntitiesByClass(FoxEntity.class, box, EntityPredicates.EXCEPT_CREATIVE_OR_SPECTATOR);
+                    for (WolfEntity wolfEntity : nearbyWolves) {
+                        if (wolfEntity.isAlive()) {
+                            if (entity == wolfEntity) {
+                                return;
+                            }
+                            wolfEntity.setTarget((LivingEntity) entity);
+                        }
+                    }
+                    for (IronGolemEntity ironGolemEntity : nearbyIronGolems) {
+                        if (ironGolemEntity.isAlive()) {
+                            if (entity == ironGolemEntity) {
+                                return;
+                            }
+                            ironGolemEntity.setTarget((LivingEntity) entity);
+                        }
+                    }
+                    for (SnowGolemEntity snowGolemEntity : nearbySnowGolems) {
+                        if (snowGolemEntity.isAlive()) {
+                            if (entity == snowGolemEntity) {
+                                return;
+                            }
+                            snowGolemEntity.setTarget((LivingEntity) entity);
+                        }
+                    }
+                    for (FoxEntity foxEntity : nearbyFoxes) {
+                        if (foxEntity.isAlive()) {
+                            if (entity == foxEntity) {
+                                return;
+                            }
+                            foxEntity.setTarget((LivingEntity) entity);
+                        }
                     }
                 }
             }
