f33926d962694e49aaf5a89265c40637e1490990
baguchan <shurf012@gmail.com>
2022-07-06 13:14:28 +0900

    scope gui adjusted

--- src/main/java/com/github/teamfusion/spyglassplus/client/event/ClientHUDEvent.java
+++ src/main/java/com/github/teamfusion/spyglassplus/client/event/ClientHUDEvent.java
@@ -9,12 +9,17 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.screens.inventory.InventoryScreen;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.MobEffectTextureManager;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.projectile.ProjectileUtil;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.ClipContext;
@@ -27,6 +32,7 @@ import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.Collection;
 import java.util.function.Predicate;
 
 @Mod.EventBusSubscriber(modid = SpyglassPlus.MOD_ID, value = Dist.CLIENT)
@@ -34,6 +40,17 @@ public class ClientHUDEvent {
 	private static final ResourceLocation SCOPE_GUI_LOCATION = new ResourceLocation(SpyglassPlus.MOD_ID, "textures/gui/scope_gui.png");
 	private static final ResourceLocation SCOPE_GUI_ICON_LOCATION = new ResourceLocation(SpyglassPlus.MOD_ID, "textures/gui/scope_gui_icons.png");
 
+	private static int eyetick;
+
+	private static boolean isEyeBlink = true;
+	private static int eyePhase;
+
+	protected static int leftPos;
+	protected static int rightPos;
+	protected static int topPos;
+
+	private static int width;
+	private static int height;
 
 	@SubscribeEvent
 	public static void renderHudEvent(RenderGameOverlayEvent.Post event) {
@@ -41,13 +58,40 @@ public class ClientHUDEvent {
 
 		Minecraft mc = Minecraft.getInstance();
 
+		if (mc.getWindow() != null) {
+			width = mc.getWindow().getGuiScaledWidth();
+			height = mc.getWindow().getGuiScaledHeight();
+			leftPos = (width) / 2;
+			rightPos = (int) ((width) / 1.5);
+			topPos = (height) / 2;
+		}
+
+		if (eyetick < 30 * 20) {
+			++eyetick;
+		} else {
+			if (isEyeBlink) {
+				eyePhase += 1;
+				if (eyePhase == 4) {
+					isEyeBlink = false;
+				}
+			} else {
+				eyePhase -= 1;
+				if (eyePhase == 0) {
+					isEyeBlink = true;
+				}
+			}
+
+			eyetick = 0;
+		}
+
 		int k = EnchantmentHelper.getItemEnchantmentLevel(SpyglassPlusEnchantments.DISCOVERY.get(), mc.player.getUseItem());
 		if (k > 0) {
 			if (event.getType() == RenderGameOverlayEvent.ElementType.ALL) {
 				stack.pushPose();
+				stack.translate((double) leftPos, (double) topPos, 0.0D);
 				Entity entity = checkEntityWithNoBlockClip(mc.player, 64.0D);
 				if (entity != null) {
-					mc.font.draw(stack, entity.getDisplayName(), (int) 20, (int) 50, 0xe0e0e0);
+					mc.font.draw(stack, entity.getDisplayName(), (int) -200, (int) -100, 0xe0e0e0);
 
 					if (entity instanceof LivingEntity) {
 
@@ -57,36 +101,82 @@ public class ClientHUDEvent {
 
 						MutableComponent s2 = new TextComponent("(  * " + ((LivingEntity) entity).getHealth() / 2 + ")").withStyle(textformatting);
 
+						stack.pushPose();
 
-						mc.font.draw(stack, s, (int) 350, (int) 50, 0xe0e0e0);
-						mc.font.draw(stack, s2, (int) 350, (int) 60, 0xe0e0e0);
+						//set right translate
+						stack.translate((double) -leftPos + rightPos, (double) 0.0F, 0.0D);
+						mc.font.draw(stack, s, (int) 20, (int) -70, 0xe0e0e0);
+						mc.font.draw(stack, s2, (int) 20, (int) -60, 0xe0e0e0);
 						RenderSystem.setShader(GameRenderer::getPositionTexShader);
 						RenderSystem.setShaderTexture(0, Gui.GUI_ICONS_LOCATION);
 
-						renderHeart(mc.gui, stack, (int) 353, (int) 60, true);
-						renderHeart(mc.gui, stack, (int) 353, (int) 60, false);
+						renderHeart(mc.gui, stack, 23, (int) -60, true);
+						renderHeart(mc.gui, stack, 23, (int) -60, false);
+
+						//attack damage
+						if (((LivingEntity) entity).getAttribute(Attributes.ATTACK_DAMAGE) != null) {
+							MutableComponent s3 = new TranslatableComponent(SpyglassPlus.MOD_ID + ".spyglass.info.damage").withStyle(textformatting);
+
+							MutableComponent s4 = new TextComponent("(  * " + ((LivingEntity) entity).getAttributeBaseValue(Attributes.ATTACK_DAMAGE) / 2 + ")").withStyle(textformatting);
+							mc.font.draw(stack, s3, (int) 20, (int) -50, 0xe0e0e0);
+							mc.font.draw(stack, s4, (int) 20, (int) -40, 0xe0e0e0);
+							RenderSystem.setShader(GameRenderer::getPositionTexShader);
+							RenderSystem.setShaderTexture(0, Gui.GUI_ICONS_LOCATION);
+
+							renderHeart(mc.gui, stack, (int) 23, (int) -40, true);
+							renderHeart(mc.gui, stack, (int) 23, (int) -40, false);
+						}
+
+						if (k > 1) {
+							Collection<MobEffectInstance> collection = ((LivingEntity) entity).getActiveEffects();
+
+							if (!collection.isEmpty()) {
+								RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+								int k2 = 33;
+								if (collection.size() > 5) {
+									k2 = 132 / (collection.size() - 1);
+								}
+
+
+								Iterable<MobEffectInstance> iterable = collection.stream().filter(net.minecraftforge.client.ForgeHooksClient::shouldRenderEffect).sorted().collect(java.util.stream.Collectors.toList());
+								renderIcons(stack, 23, -(k2 - 8), 6, iterable);
+							}
+						}
+
+						//reset
+						stack.popPose();
+
 
 						//entity and gui
 						//idk why I should have to double
 						stack.pushPose();
-						stack.scale(0.5F, 0.5F, 0.5F);
+						stack.scale(0.75F, 0.75F, 0.75F);
 						RenderSystem.setShader(GameRenderer::getPositionTexColorShader);
 						RenderSystem.setShaderTexture(0, SCOPE_GUI_LOCATION);
 						RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-						mc.gui.blit(stack, 0, 130, 0, 0, 64 * 2, 128 * 2);
+						mc.gui.blit(stack, -308, -80, 0, 0, 64 * 2, 124 * 2);
+						mc.gui.blit(stack, -280, -80, 128, (32 * eyePhase * 1), 64, 32);
+						//render entity
 						stack.popPose();
-						stack.pushPose();
-						stack.scale(0.65F, 0.65F, 0.65F);
-						RenderSystem.setShaderTexture(0, SCOPE_GUI_ICON_LOCATION);
-						mc.gui.blit(stack, 32, 100, 32 * (5 - k) * 1 - 2, 0, 32 * 1, 16 * 2);
-						stack.popPose();
-						InventoryScreen.renderEntityInInventory(30, 200, 20, 0.0F, 0.0F, (LivingEntity) entity);
-
+						InventoryScreen.renderEntityInInventory(leftPos + -180, topPos + 90, 20, 0.0F, 0.0F, (LivingEntity) entity);
 					}
 				}
 				stack.popPose();
 			}
 		}
+	}
+
+	private static void renderIcons(PoseStack p_194009_, int x, int y, int offsetX, Iterable<MobEffectInstance> p_194012_) {
+		int i = 0;
+		MobEffectTextureManager mobeffecttexturemanager = Minecraft.getInstance().getMobEffectTextures();
+		for (MobEffectInstance mobeffectinstance : p_194012_) {
+			MobEffect mobeffect = mobeffectinstance.getEffect();
+			TextureAtlasSprite textureatlassprite = mobeffecttexturemanager.get(mobeffect);
+			RenderSystem.setShaderTexture(0, textureatlassprite.atlas().location());
+			Minecraft.getInstance().gui.blit(p_194009_, x + 6 + i, y, 0, 18, 18, textureatlassprite);
+			i += offsetX;
+		}
+
 	}
 
 	private static void renderHeart(Gui gui, PoseStack posestack, int x, int y, boolean isContainer) {
