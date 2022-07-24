a2eefe9c447ff1b3019bf6b7862d408b48a9fa8f
andantet <orrdaniel73@gmail.com>
2022-07-24 02:17:31 +0100

    stretch binoculars overlay

--- src/main/java/com/github/teamfusion/spyglassplus/mixin/client/InGameHudMixin.java
+++ src/main/java/com/github/teamfusion/spyglassplus/mixin/client/InGameHudMixin.java
@@ -1,17 +1,20 @@
 package com.github.teamfusion.spyglassplus.mixin.client;
 
 import com.github.teamfusion.spyglassplus.api.SpyglassPlus;
+import com.github.teamfusion.spyglassplus.api.item.BinocularsItem;
+import com.github.teamfusion.spyglassplus.api.item.SpyglassPlusItems;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.Tessellator;
 import net.minecraft.client.render.VertexFormat;
 import net.minecraft.client.render.VertexFormats;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -21,71 +24,70 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
 @Mixin(InGameHud.class)
-public class InGameHudMixin {
+public abstract class InGameHudMixin {
     @Unique private static final Identifier BINOCULARS_SCOPE_TEXTURE = new Identifier(SpyglassPlus.MOD_ID, "textures/misc/binoculars_scope.png");
-    @Shadow private float spyglassScale;
 
+    @Shadow @Final private MinecraftClient client;
     @Shadow private int scaledWidth;
-
     @Shadow private int scaledHeight;
 
-    @Inject(
-        method = "render",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/client/gui/hud/InGameHud;renderSpyglassOverlay(F)V",
-            shift = At.Shift.AFTER
-        )
-    )
-    private void onCallRenderSpyglassOverlay(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
-        this.renderBinocularsOverlay(this.spyglassScale);
+    @Inject(method = "renderSpyglassOverlay", at = @At("HEAD"), cancellable = true)
+    private void onRenderSpyglassOverlay(float scale, CallbackInfo ci) {
+        if (this.client.player.getActiveItem().getItem() instanceof BinocularsItem) {
+            this.renderBinocularsOverlay(scale);
+            ci.cancel();
+        }
     }
 
     @Unique
     private void renderBinocularsOverlay(float scale) {
-        float f;
         RenderSystem.disableDepthTest();
         RenderSystem.depthMask(false);
         RenderSystem.defaultBlendFunc();
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderTexture(0, BINOCULARS_SCOPE_TEXTURE);
+
         Tessellator tessellator = Tessellator.getInstance();
-        BufferBuilder bufferBuilder = tessellator.getBuffer();
-        float g = f = (float)Math.min(this.scaledWidth, this.scaledHeight);
-        float h = Math.min((float)this.scaledWidth / f, (float)this.scaledHeight / g) * scale;
-        float i = f * h;
-        float j = g * h;
-        float k = ((float)this.scaledWidth - i) / 2.0f;
-        float l = ((float)this.scaledHeight - j) / 2.0f;
-        float m = k + i;
-        float n = l + j;
-        bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
-        bufferBuilder.vertex(k, n, -90.0).texture(0.0f, 1.0f).next();
-        bufferBuilder.vertex(m, n, -90.0).texture(1.0f, 1.0f).next();
-        bufferBuilder.vertex(m, l, -90.0).texture(1.0f, 0.0f).next();
-        bufferBuilder.vertex(k, l, -90.0).texture(0.0f, 0.0f).next();
+        BufferBuilder buffer = tessellator.getBuffer();
+        float ws;
+        float hs = ws = (float)Math.min(this.scaledWidth, this.scaledHeight);
+        float h = Math.min((float)this.scaledWidth / ws, (float)this.scaledHeight / hs) * scale;
+        float hori = ws * h * 1.82F;
+        float vert = hs * h * 0.95F;
+        float left = ((float)this.scaledWidth - hori) / 2.0f;
+        float top = ((float)this.scaledHeight - vert) / 2.0f;
+        float right = left + hori;
+        float bottom = top + vert;
+        buffer.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
+        buffer.vertex(left, bottom, -90.0).texture(0.0f, 1.0f).next();  // bottom left
+        buffer.vertex(right, bottom, -90.0).texture(1.0f, 1.0f).next(); // bottom right
+        buffer.vertex(right, top, -90.0).texture(1.0f, 0.0f).next();    // top right
+        buffer.vertex(left, top, -90.0).texture(0.0f, 0.0f).next();     // top left
         tessellator.draw();
+
         RenderSystem.setShader(GameRenderer::getPositionColorShader);
         RenderSystem.disableTexture();
-        bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
-        bufferBuilder.vertex(0.0, this.scaledHeight, -90.0).color(0, 0, 0, 255).next();
-        bufferBuilder.vertex(this.scaledWidth, this.scaledHeight, -90.0).color(0, 0, 0, 255).next();
-        bufferBuilder.vertex(this.scaledWidth, n, -90.0).color(0, 0, 0, 255).next();
-        bufferBuilder.vertex(0.0, n, -90.0).color(0, 0, 0, 255).next();
-        bufferBuilder.vertex(0.0, l, -90.0).color(0, 0, 0, 255).next();
-        bufferBuilder.vertex(this.scaledWidth, l, -90.0).color(0, 0, 0, 255).next();
-        bufferBuilder.vertex(this.scaledWidth, 0.0, -90.0).color(0, 0, 0, 255).next();
-        bufferBuilder.vertex(0.0, 0.0, -90.0).color(0, 0, 0, 255).next();
-        bufferBuilder.vertex(0.0, n, -90.0).color(0, 0, 0, 255).next();
-        bufferBuilder.vertex(k, n, -90.0).color(0, 0, 0, 255).next();
-        bufferBuilder.vertex(k, l, -90.0).color(0, 0, 0, 255).next();
-        bufferBuilder.vertex(0.0, l, -90.0).color(0, 0, 0, 255).next();
-        bufferBuilder.vertex(m, n, -90.0).color(0, 0, 0, 255).next();
-        bufferBuilder.vertex(this.scaledWidth, n, -90.0).color(0, 0, 0, 255).next();
-        bufferBuilder.vertex(this.scaledWidth, l, -90.0).color(0, 0, 0, 255).next();
-        bufferBuilder.vertex(m, l, -90.0).color(0, 0, 0, 255).next();
+        buffer.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
+        buffer.vertex(0.0, this.scaledHeight, -90.0).color(0, 0, 0, 255).next();
+        buffer.vertex(this.scaledWidth, this.scaledHeight, -90.0).color(0, 0, 0, 255).next();
+        buffer.vertex(this.scaledWidth, bottom, -90.0).color(0, 0, 0, 255).next();
+        buffer.vertex(0.0, bottom, -90.0).color(0, 0, 0, 255).next();
+        buffer.vertex(0.0, top, -90.0).color(0, 0, 0, 255).next();
+        buffer.vertex(this.scaledWidth, top, -90.0).color(0, 0, 0, 255).next();
+        buffer.vertex(this.scaledWidth, 0.0, -90.0).color(0, 0, 0, 255).next();
+        buffer.vertex(0.0, 0.0, -90.0).color(0, 0, 0, 255).next();
+        buffer.vertex(0.0, bottom, -90.0).color(0, 0, 0, 255).next();
+        buffer.vertex(left, bottom, -90.0).color(0, 0, 0, 255).next();
+        buffer.vertex(left, top, -90.0).color(0, 0, 0, 255).next();
+        buffer.vertex(0.0, top, -90.0).color(0, 0, 0, 255).next();
+        buffer.vertex(right, bottom, -90.0).color(0, 0, 0, 255).next();
+        buffer.vertex(this.scaledWidth, bottom, -90.0).color(0, 0, 0, 255).next();
+        buffer.vertex(this.scaledWidth, top, -90.0).color(0, 0, 0, 255).next();
+        buffer.vertex(right, top, -90.0).color(0, 0, 0, 255).next();
         tessellator.draw();
         RenderSystem.enableTexture();
+
+        RenderSystem.setShader(GameRenderer::getPositionColorShader);
         RenderSystem.depthMask(true);
         RenderSystem.enableDepthTest();
         RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
