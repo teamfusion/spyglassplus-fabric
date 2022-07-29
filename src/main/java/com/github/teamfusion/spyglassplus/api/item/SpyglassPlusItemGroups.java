0ee6dbf3d0849bee8a9376cf39f25382e520969e
andantet <orrdaniel73@gmail.com>
2022-07-29 11:31:16 +0100

    fix enchanted books in the creative tab not working

--- src/main/java/com/github/teamfusion/spyglassplus/api/item/SpyglassPlusItemGroups.java
+++ src/main/java/com/github/teamfusion/spyglassplus/api/item/SpyglassPlusItemGroups.java
@@ -1,30 +1,16 @@
 package com.github.teamfusion.spyglassplus.api.item;
 
 import com.github.teamfusion.spyglassplus.api.SpyglassPlus;
-import com.github.teamfusion.spyglassplus.api.enchantment.ScopingEnchantment;
+import com.github.teamfusion.spyglassplus.api.enchantment.target.SpyglassPlusEnchantmentTargets;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.collection.DefaultedList;
-import net.minecraft.util.registry.Registry;
 
 public interface SpyglassPlusItemGroups {
     ItemGroup ALL = FabricItemGroupBuilder.create(new Identifier(SpyglassPlus.MOD_ID, "item_group"))
                                           .icon(() -> new ItemStack(Items.SPYGLASS))
-                                          .appendItems((list, group) -> {
-                                              DefaultedList<ItemStack> stacks = (DefaultedList<ItemStack>) list;
-
-                                              Registry.ITEM.forEach(item -> item.appendStacks(group, stacks));
-
-                                              Registry.ENCHANTMENT.stream()
-                                                                  .filter(ScopingEnchantment.class::isInstance)
-                                                                  .forEach(enchantment -> {
-                                                                      ItemStack stack = new ItemStack(Items.ENCHANTED_BOOK);
-                                                                      stack.addEnchantment(enchantment, enchantment.getMaxLevel());
-                                                                      stacks.add(stack);
-                                                                  });
-                                          })
-                                          .build();
+                                          .build()
+                                          .setEnchantments(SpyglassPlusEnchantmentTargets.SCOPING);
 }
